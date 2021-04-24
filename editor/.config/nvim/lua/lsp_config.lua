1a3ba863081d210c8a8489d039c5790de19d7fd6
Jason Rodney Hansen <jasonrodneyhansen@gmail.com>
2021-04-23 22:48:22 -0600

    Add LspRestart and LspLog commands

--- editor/.config/nvim/lua/lsp_config.lua
+++ editor/.config/nvim/lua/lsp_config.lua
@@ -232,6 +232,20 @@ lsp_status.config({
   spinner_frames = {'⣾', '⣽', '⣻', '⢿', '⡿', '⣟', '⣯', '⣷'},
 })
 
+-- Add LspLog command
+function _G.reload_lsp()
+  vim.lsp.stop_client(vim.lsp.get_active_clients())
+  vim.cmd [[edit]]
+end
+vim.cmd('command! -nargs=0 LspLog call v:lua.open_lsp_log()')
+
+-- Add LspRestart command
+function _G.open_lsp_log()
+  local path = vim.lsp.get_log_path()
+  vim.cmd("edit " .. path)
+end
+vim.cmd('command! -nargs=0 LspRestart call v:lua.reload_lsp()')
+
 return {
   toggle_diagnostic_virtual_text = toggle_diagnostic_virtual_text,
   sign_error = sign_error,
