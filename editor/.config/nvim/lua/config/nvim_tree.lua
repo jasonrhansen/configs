5c488bb46f6579f9377672f00933384754ab185b
Jason Rodney Hansen <jasonrodneyhansen@gmail.com>
2021-05-22 19:53:01 -0600

    Changes to nvim_tree.config

--- editor/.config/nvim/lua/config/nvim_tree.lua
+++ editor/.config/nvim/lua/config/nvim_tree.lua
@@ -6,8 +6,9 @@ vim.g.nvim_tree_ignore = {".git", "node_modules", ".cache"}
 vim.g.nvim_tree_auto_close = 1
 vim.g.nvim_tree_follow = 1
 vim.g.nvim_tree_indent_markers = 1
-vim.g.nvim_tree_hide_dotfiles = 1
-vim.g.nvim_tree_git_hl = 1
+vim.g.nvim_tree_hide_dotfiles = 0
+vim.g.nvim_tree_git_hl = 0
+vim.g.nvim_tree_lsp_diagnostics = 1
 
 vim.g.nvim_tree_show_icons = {
   git = 1,
