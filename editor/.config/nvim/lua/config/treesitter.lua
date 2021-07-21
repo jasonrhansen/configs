f4feda518fa07f6c12fe758d190a21e37affab5e
Jason Rodney Hansen <jasonrodneyhansen@gmail.com>
2021-07-20 20:55:55 -0600

    Add treesitter query linter

--- editor/.config/nvim/lua/config/treesitter.lua
+++ editor/.config/nvim/lua/config/treesitter.lua
@@ -90,7 +90,12 @@ treesitter.setup {
       goto_node = '<cr>',
       show_help = '?',
     },
-  }
+  },
+  query_linter = {
+    enable = true,
+    use_virtual_text = true,
+    lint_events = {"BufWrite", "CursorHold"},
+  },
 }
 
 vim.api.nvim_set_keymap("", "<leader>T", "<cmd>TSBufToggle highlight<CR>", {noremap=true, silent=true})
