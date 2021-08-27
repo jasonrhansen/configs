eede07617940b9b7c32dfdec616ab76b8dfba502
Jason Rodney Hansen <jasonrodneyhansen@gmail.com>
2021-08-27 14:19:29 -0600

    Allow to use Tab for indentation

--- editor/.config/nvim/lua/config/nvim_cmp.lua
+++ editor/.config/nvim/lua/config/nvim_cmp.lua
@@ -2,6 +2,11 @@ local cmp = require("cmp")
 
 local M = {}
 
+local check_back_space = function()
+  local col = vim.fn.col(".") - 1
+  return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
+end
+
 cmp.setup({
   snippet = {
     expand = function(args)
@@ -26,6 +31,8 @@ cmp.setup({
     ["<Tab>"] = function(fallback)
       if vim.fn.pumvisible() == 1 then
         vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<C-n>", true, true, true), "n")
+      elseif check_back_space() then
+        vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, true, true), "n")
       elseif vim.fn["vsnip#available"]() == 1 then
         vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>(vsnip-expand-or-jump)", true, true, true), "")
       else
