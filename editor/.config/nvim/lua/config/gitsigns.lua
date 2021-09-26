76b0f32a8be0d5130e1bdfed815a1b1540419eeb
Jason Rodney Hansen <jasonrodneyhansen@gmail.com>
2021-09-26 14:20:24 -0600

    Updates to gitsigns config

--- editor/.config/nvim/lua/config/gitsigns.lua
+++ editor/.config/nvim/lua/config/gitsigns.lua
@@ -27,13 +27,14 @@ require("gitsigns").setup({
     ["o ih"] = ":<C-U>lua require('gitsigns').select_hunk()<CR>",
     ["x ih"] = ":<C-U>lua require('gitsigns').select_hunk()<CR>",
   },
-  watch_index = {
+  watch_gitdir = {
     interval = 1000,
   },
   current_line_blame = true,
   sign_priority = 6,
   update_debounce = 100,
   status_formatter = nil, -- Use default
-  use_decoration_api = true,
-  use_internal_diff = true, -- If luajit is present
+  diff_opts = {
+    internal = true,
+  },
 })
