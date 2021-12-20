53482b51ea891dbce2ec1affe286034f3da6db67
Jason Rodney Hansen <jasonrodneyhansen@gmail.com>
2021-12-20 16:00:59 -0700

    Fix for vim-oscyank config

--- editor/.config/nvim/lua/plugins.lua
+++ editor/.config/nvim/lua/plugins.lua
@@ -274,7 +274,7 @@ use({
 use({
   "ojroques/vim-oscyank",
   config = function()
-    vim.cmd[[autocmd TextYankPost * if v:event.operator is 'y' && v:event.regname is '' | execute 'OSCYankReg "' | endif"]]
+    vim.cmd[[autocmd TextYankPost * if v:event.operator is 'y' && (v:event.regname is '' || v:event.regname is '+') | execute 'OSCYankReg "' | endif"]]
     vim.g.oscyank_silent = true
   end,
 })
