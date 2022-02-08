fafac7f4e6e21974e796c5cc4f6cdd924e7a94bb
Jason Rodney Hansen <jasonrodneyhansen@gmail.com>
2022-02-08 14:50:08 -0700

    Change keybindings for typescript

--- editor/.config/nvim/lua/config/lsp.lua
+++ editor/.config/nvim/lua/config/lsp.lua
@@ -159,7 +159,8 @@ local configs = {
       -- no default maps, so you may want to define some here
       local opts = { silent = true }
       vim.api.nvim_buf_set_keymap(bufnr, "n", "gs", ":TSLspOrganize<CR>", opts)
-      vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", ":TSLspRenameFile<CR>", opts)
+      vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>rn", ":TSLspRenameFile<CR>", opts)
+      vim.api.nvim_buf_set_keymap(bufnr, "n", "<F2>", ":TSLspRenameFile<CR>", opts)
       vim.api.nvim_buf_set_keymap(bufnr, "n", "gi", ":TSLspImportAll<CR>", opts)
     end,
   },
