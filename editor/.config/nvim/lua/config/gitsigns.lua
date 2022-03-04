02562c4bc41df6f63d4df8d6246b8601034b2850
Jason Rodney Hansen <jasonrodneyhansen@gmail.com>
2022-03-04 12:28:39 -0700

    Configure gitsigns keymaps with which-key

--- editor/.config/nvim/lua/config/gitsigns.lua
+++ editor/.config/nvim/lua/config/gitsigns.lua
@@ -8,25 +8,6 @@ require("gitsigns").setup({
   },
   numhl = false,
   linehl = false,
-  keymaps = {
-    -- Default keymap options
-    noremap = true,
-    buffer = true,
-
-    ["n ]c"] = { expr = true, [[&diff ? "]c" : "<cmd>lua require('gitsigns').next_hunk()<CR>"]] },
-    ["n [c"] = { expr = true, [[&diff ? "[c" : "<cmd>lua require('gitsigns').prev_hunk()<CR>"]] },
-
-    ["n <leader>hs"] = "<cmd>lua require('gitsigns').stage_hunk()<CR>",
-    ["n <leader>hu"] = "<cmd>lua require('gitsigns').undo_stage_hunk()<CR>",
-    ["n <leader>hr"] = "<cmd>lua require('gitsigns').reset_hunk()<CR>",
-    ["n <leader>hR"] = "<cmd>lua require('gitsigns').reset_buffer()<CR>",
-    ["n <leader>hp"] = "<cmd>lua require('gitsigns').preview_hunk()<CR>",
-    ["n <leader>hb"] = "<cmd>lua require('gitsigns').blame_line()<CR>",
-
-    -- Text objects
-    ["o ih"] = ":<C-U>lua require('gitsigns').select_hunk()<CR>",
-    ["x ih"] = ":<C-U>lua require('gitsigns').select_hunk()<CR>",
-  },
   watch_gitdir = {
     interval = 1000,
   },
@@ -37,4 +18,37 @@ require("gitsigns").setup({
   diff_opts = {
     internal = true,
   },
+  on_attach = function(bufnr)
+    local normal_keymaps = {
+      ["]c"] = { [[&diff ? "]c" : "<cmd>lua require('gitsigns').next_hunk()<CR>"]], "Jump to next hunk", expr = true },
+      ["[c"] = {
+        [[&diff ? "[c" : "<cmd>lua require('gitsigns').prev_hunk()<CR>"]],
+        "Jump to previous hunk",
+        expr = true,
+      },
+
+      ["<leader>h"] = {
+        name = "Gitsigns",
+        ["s"] = { "<cmd>lua require('gitsigns').stage_hunk()<CR>", "Stage hunk" },
+        ["u"] = { "<cmd>lua require('gitsigns').undo_stage_hunk()<CR>", "Unstage hunk" },
+        ["r"] = { "<cmd>lua require('gitsigns').reset_hunk()<CR>", "Reset hunk" },
+        ["R"] = { "<cmd>lua require('gitsigns').reset_buffer()<CR>", "Reset buffer" },
+        ["p"] = { "<cmd>lua require('gitsigns').preview_hunk()<CR>", "Preview hunk" },
+        ["b"] = { "<cmd>lua require('gitsigns').blame_line()<CR>", "Blame line" },
+      },
+    }
+
+    local wk = require("which-key")
+    wk.register(normal_keymaps, { buffer = bufnr })
+
+    -- Text objects
+    wk.register(
+      { ["ih"] = { ":<C-U>lua require('gitsigns').select_hunk()<CR>", "Select hunk" } },
+      { mode = "o", buffer = bufnr }
+    )
+    wk.register(
+      { ["ih"] = { ":<C-U>lua require('gitsigns').select_hunk()<CR>", "Select hunk" } },
+      { mode = "x", buffer = bufnr }
+    )
+  end,
 })
