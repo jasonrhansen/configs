5306c356610556e967c8676cee0e6cdd86a98e3c
Jason Rodney Hansen <jasonrodneyhansen@gmail.com>
2022-07-28 11:10:42 -0600

    Fixes for winbar config

--- editor/.config/nvim/lua/config/winbar.lua
+++ editor/.config/nvim/lua/config/winbar.lua
@@ -1,7 +1,18 @@
 vim.opt_global.winbar = nil
 
+local ignore_filetypes = {
+  '',
+  'neo-tree',
+}
+
 local function set_winbar(highlight)
-  if vim.opt.filetype:get() ~= '' and vim.opt.buftype:get() == '' then
+  local ignored_filetype = vim.tbl_contains(ignore_filetypes, vim.opt.filetype:get())
+  if ignored_filetype then return end
+
+  local is_floating = vim.api.nvim_win_get_config(0).relative ~= ''
+  if is_floating then return end
+
+  if vim.opt.buftype:get() == '' then
     vim.opt_local.winbar = "%#" .. highlight .. "#%=%m %f"
   else
     vim.opt_local.winbar = nil
