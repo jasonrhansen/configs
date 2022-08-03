4b5fbf2676722e70c78023b39fb44aead890425e
Jason Rodney Hansen <jasonrodneyhansen@gmail.com>
2022-08-03 09:00:03 -0600

    Fix OSC 52 config

--- editor/.config/nvim/lua/config/nvim_osc52.lua
+++ editor/.config/nvim/lua/config/nvim_osc52.lua
@@ -1,19 +1,18 @@
-require("osc52").setup({
+local osc52 = require("osc52")
+
+osc52.setup({
   max_length = 0, -- Maximum length of selection (0 for no limit)
-  silent = false, -- Disable message on successful copy
+  silent = true, -- Disable message on successful copy
   trim = false, -- Trim text before copy
 })
 
-local function copy(lines, _)
-  require("osc52").copy(table.concat(lines, "\n"))
-end
-
-local function paste()
-  return { vim.fn.split(vim.fn.getreg(""), "\n"), vim.fn.getregtype("") }
-end
-
-vim.g.clipboard = {
-  name = "osc52",
-  copy = { ["+"] = copy, ["*"] = copy },
-  paste = { ["+"] = paste, ["*"] = paste },
-}
+vim.api.nvim_create_autocmd({ "TextYankPost" }, {
+  group = "jason-config",
+  callback = function(_)
+    local operator = vim.v.event.operator
+    local regname = vim.v.event.regname
+    if operator == "y" and (regname == "" or regname == "+") then
+      osc52.copy(vim.fn.getreg('"'))
+    end
+  end,
+})
