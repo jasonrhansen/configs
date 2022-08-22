2d805e84a133a90ea51a158142a6e2c288987b80
Jason Rodney Hansen <jasonrodneyhansen@gmail.com>
2022-08-22 16:50:54 -0600

    Fix brace autopair for ruby files

--- editor/.config/nvim/lua/config/nvim_autopairs.lua
+++ editor/.config/nvim/lua/config/nvim_autopairs.lua
@@ -1,5 +1,6 @@
 local autopairs = require("nvim-autopairs")
 local Rule = require("nvim-autopairs.rule")
+local cond = require('nvim-autopairs.conds')
 
 autopairs.setup({})
 
@@ -7,7 +8,7 @@ autopairs.setup({})
 autopairs.remove_rule("{")
 autopairs.add_rules({
   Rule("{", "}", "-ruby"):end_wise(),
-  Rule("{", "}", "ruby"),
+  Rule("{", "}", "ruby"):with_move(cond.move_right()),
 })
 
 -- Arrow functions in javascript/typescript.
