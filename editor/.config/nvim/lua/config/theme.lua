4901d9931fc6bbbc9e7627a93be609c20195c2c7
Jason Rodney Hansen <jasonrodneyhansen@gmail.com>
2022-08-26 16:32:48 -0600

    Remove kanagawa color override

--- editor/.config/nvim/lua/config/theme.lua
+++ editor/.config/nvim/lua/config/theme.lua
@@ -1,11 +1,5 @@
 vim.g.tokyonight_style = "night"
 vim.g.tokyonight_colors = { border = "#292E42" }
 
-local colors = require("kanagawa.colors").setup()
-require("kanagawa").setup({
-  overrides = {
-    VertSplit = { fg = colors.sumiInk4, bg = colors.bg },
-  }
-})
-
+require("kanagawa").setup({})
 vim.cmd.colorscheme('kanagawa')
