09983bf21e362ea2d02103cdc8c6cd568a903474
Jason Rodney Hansen <jasonrodneyhansen@gmail.com>
2022-09-26 16:55:49 -0600

    Remove autopair for arrow functions

--- editor/.config/nvim/lua/config/nvim_autopairs.lua
+++ editor/.config/nvim/lua/config/nvim_autopairs.lua
@@ -1,11 +1,3 @@
 local autopairs = require("nvim-autopairs")
-local Rule = require("nvim-autopairs.rule")
 
 autopairs.setup({})
-
--- Arrow functions in javascript/typescript.
-autopairs.add_rule(
-  Rule("%(.*%)%s*%=>$", " {  }", { "typescript", "typescriptreact", "javascript" })
-    :use_regex(true)
-    :set_end_pair_length(2)
-)
