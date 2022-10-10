eba02a47fd79abe8cd997cef1c54badac9b1f05b
Jason Rodney Hansen <jasonrodneyhansen@gmail.com>
2022-10-10 10:36:41 -0600

    Simplify pick_window_and_do

--- editor/.config/nvim/lua/config/util.lua
+++ editor/.config/nvim/lua/config/util.lua
@@ -1,11 +1,10 @@
 local M = {}
 
 M.pick_window_and_do = function(func)
-  local win = vim.api.nvim_get_current_win();
   local buf = vim.api.nvim_get_current_buf();
   local cursor = vim.api.nvim_win_get_cursor(0);
-  local picked_win = require('window-picker').pick_window() or vim.api.nvim_get_current_win()
-  if picked_win ~= win then
+  local picked_win = require('window-picker').pick_window()
+  if picked_win then
     vim.api.nvim_set_current_win(picked_win)
     vim.api.nvim_set_current_buf(buf)
     vim.api.nvim_win_set_cursor(picked_win, cursor)
