85a8abbaa2d6ba500c2f47ed1c91ce4eaeb77d0f
Jason Rodney Hansen <jasonrodneyhansen@gmail.com>
2022-10-09 18:44:09 -0600

    Add ability to jump to things in other windows

---
+++ editor/.config/nvim/lua/config/util.lua
@@ -0,0 +1,18 @@
+local M = {}
+
+M.pick_window_and_do = function(func)
+  local win = vim.api.nvim_get_current_win();
+  local buf = vim.api.nvim_get_current_buf();
+  local cursor = vim.api.nvim_win_get_cursor(0);
+  local picked_win = require('window-picker').pick_window() or vim.api.nvim_get_current_win()
+  if picked_win ~= win then
+    vim.api.nvim_set_current_win(picked_win)
+    vim.api.nvim_set_current_buf(buf)
+    vim.api.nvim_win_set_cursor(picked_win, cursor)
+  else
+    vim.cmd.vsplit()
+  end
+  func()
+end
+
+return M
