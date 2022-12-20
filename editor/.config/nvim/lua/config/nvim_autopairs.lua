1e3875d342ddf39f7a5fd4e1d829d714e73e21b6
Jason Rodney Hansen <jasonrodneyhansen@gmail.com>
2022-12-20 15:20:14 -0700

    Fix for CR with cmp and autopairs

--- editor/.config/nvim/lua/config/nvim_autopairs.lua
+++ editor/.config/nvim/lua/config/nvim_autopairs.lua
@@ -1,3 +1,7 @@
 local autopairs = require("nvim-autopairs")
 
 autopairs.setup({})
+
+local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+local cmp = require("cmp")
+cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
