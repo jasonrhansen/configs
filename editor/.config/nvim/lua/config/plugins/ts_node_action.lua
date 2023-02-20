05944d21c89263fcc347240854e2c409b34a81a6
Jason Rodney Hansen <jasonrodneyhansen@gmail.com>
2023-02-20 16:12:07 -0700

    Add ability to expand Ruby method chains to multiple lines

--- editor/.config/nvim/lua/config/plugins/ts_node_action.lua
+++ editor/.config/nvim/lua/config/plugins/ts_node_action.lua
@@ -4,12 +4,44 @@
 --    * Toggle block (Ruby)
 --    * Toggle hash style (Ruby)
 --    * Toggle hash style (Ruby)
+
+local function multiline_method_chain(node)
+  local helpers = require('ts-node-action.helpers')
+  local replacement = {}
+
+  for child in node:iter_children() do
+    if child:type() == "call" then
+      for _, r in ipairs(multiline_method_chain(child)) do
+        table.insert(replacement, r)
+      end
+    else
+      local node_text = helpers.node_text(child)
+      if node_text == "."  or next(replacement) == nil then
+        table.insert(replacement, node_text)
+      else
+        if node_text:sub(1, 1) == "{" then
+          node_text = " " .. node_text
+        end
+        replacement[#replacement] = replacement[#replacement] .. node_text
+      end
+    end
+  end
+
+  return replacement, { cursor = {}, format = true }
+end
+
 return {
   "ckolkey/ts-node-action",
   dependencies = { "nvim-treesitter", "tpope/vim-repeat" },
   config = function()
     local ts_node_action = require("ts-node-action")
-    ts_node_action.setup({})
+    ts_node_action.setup({
+      ruby = {
+        ['call'] = {
+          { multiline_method_chain, "Multi-line method chain" },
+        },
+      },
+    })
 
     vim.keymap.set({ "n" }, "<leader>A", ts_node_action.node_action, { desc = "Trigger Node Action" })
 
