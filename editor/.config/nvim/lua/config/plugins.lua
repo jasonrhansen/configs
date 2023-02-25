f774b6dc27bc71a09ff6449a2548a24c64ab5f5b
Jason Rodney Hansen <jasonrodneyhansen@gmail.com>
2023-02-25 12:57:28 -0700

    No more "kj" to exit insert mode

--- editor/.config/nvim/lua/config/plugins.lua
+++ editor/.config/nvim/lua/config/plugins.lua
@@ -209,7 +209,7 @@ return {
     "max397574/better-escape.nvim",
     config = function()
       require("better_escape").setup({
-        mapping = { "jk", "kj" },
+        mapping = { "jk" },
         timeout = vim.o.timeoutlen,
         clear_empty_lines = true,
         keys = function()
