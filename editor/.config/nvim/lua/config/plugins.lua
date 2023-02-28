9a0e264ab2f3158101fadf0d5df934afab5b4a07
Jason Rodney Hansen <jasonrodneyhansen@gmail.com>
2023-02-27 21:19:44 -0700

    Set log_level = "error" for auto-session

--- editor/.config/nvim/lua/config/plugins.lua
+++ editor/.config/nvim/lua/config/plugins.lua
@@ -170,7 +170,9 @@ return {
   {
     "rmagatti/auto-session",
     config = function()
-      require("auto-session").setup({})
+      require("auto-session").setup({
+        log_level = "error",
+      })
     end,
   },
   {
