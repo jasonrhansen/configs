f7fbfa24301565ab4621166318c0303f55e271f1
Jason Rodney Hansen <jasonrodneyhansen@gmail.com>
2023-03-07 15:44:06 -0700

    Customize kanagawa to remove bg color from gutter

--- editor/.config/nvim/lua/config/theme.lua
+++ editor/.config/nvim/lua/config/theme.lua
@@ -1,5 +1,15 @@
 vim.g.tokyonight_style = "night"
 vim.g.tokyonight_colors = { border = "#292E42" }
 
-require("kanagawa").setup({})
-vim.cmd.colorscheme('kanagawa')
+require("kanagawa").setup({
+  colors = {
+    theme = {
+      all = {
+        ui = {
+          bg_gutter = "none",
+        },
+      },
+    },
+  },
+})
+vim.cmd.colorscheme("kanagawa")
