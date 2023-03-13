4e40637168f2d75006f711f191207bb537be3b56
Jason Rodney Hansen <jasonrodneyhansen@gmail.com>
2023-03-12 18:50:46 -0600

    Autopairs rules for rust raw string literals

--- editor/.config/nvim/lua/config/plugins/autopairs.lua
+++ editor/.config/nvim/lua/config/plugins/autopairs.lua
@@ -2,11 +2,18 @@ return {
   "windwp/nvim-autopairs",
   config = function()
     local autopairs = require("nvim-autopairs")
-
     autopairs.setup({})
+
+    local Rule = require("nvim-autopairs.rule")
+
+    autopairs.add_rules({
+      Rule('r#"', '"#', "rust"),
+      Rule('r##"', '"##', "rust"),
+      Rule('r###"', '"###', "rust"),
+    })
 
     local cmp_autopairs = require("nvim-autopairs.completion.cmp")
     local cmp = require("cmp")
     cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-  end
+  end,
 }
