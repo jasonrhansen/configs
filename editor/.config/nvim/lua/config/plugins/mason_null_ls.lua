6e8f648268830399a62ae4d5cd289b1238a888e4
Jason Rodney Hansen <jasonrodneyhansen@gmail.com>
2023-04-11 18:00:22 -0600

    Update mason_null_ls config for newer version

--- editor/.config/nvim/lua/config/plugins/mason_null_ls.lua
+++ editor/.config/nvim/lua/config/plugins/mason_null_ls.lua
@@ -6,23 +6,19 @@ return {
     "jose-elias-alvarez/null-ls.nvim",
   },
   config = function()
+    local null_ls = require("null-ls")
+
     require("mason-null-ls").setup({
       ensure_installed = { "prettierd", "stylua", "sql_formatter" },
       automatic_installation = false,
       automatic_setup = true,
-    })
-
-    local null_ls = require("null-ls")
-
-    require("mason-null-ls").setup_handlers({
-      function(source_name, methods)
-        require("mason-null-ls.automatic_setup")(source_name, methods)
-      end,
-      stylua = function()
-        null_ls.register(null_ls.builtins.formatting.stylua.with({
-          args = { "-s", "--indent-type", "Spaces", "--indent-width", "2", "-" },
-        }))
-      end,
+      handlers = {
+        stylua = function()
+          null_ls.register(null_ls.builtins.formatting.stylua.with({
+            args = { "-s", "--indent-type", "Spaces", "--indent-width", "2", "-" },
+          }))
+        end,
+      },
     })
   end,
 }
