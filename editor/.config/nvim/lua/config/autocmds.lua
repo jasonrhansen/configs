c080f6d172a916d82ab3f9063990a7af1c9e21a6
Jason Rodney Hansen <jasonrodneyhansen@gmail.com>
2023-06-20 08:46:38 -0600

    Don't rename tmux windows in neovim because it's causing issues when doing git commits

--- editor/.config/nvim/lua/config/autocmds.lua
+++ editor/.config/nvim/lua/config/autocmds.lua
@@ -35,25 +35,6 @@ vim.api.nvim_create_autocmd({ "BufReadPost" }, {
   end,
 })
 
--- Automatically set tmux window name.
-if vim.fn.exists("$TMUX") == 1 and vim.fn.exists("$NORENAME") == 0 then
-  vim.api.nvim_create_autocmd({ "BufEnter" }, {
-    group = "jason-config",
-    callback = function()
-      if vim.o.buftype == "" then
-        vim.fn.system("tmux rename-window " .. vim.fn.expand("%:t:S"))
-      end
-    end,
-  })
-
-  vim.api.nvim_create_autocmd({ "VimLeave" }, {
-    group = "jason-config",
-    callback = function()
-      vim.fn.system("tmux set-window automatic-rename on")
-    end,
-  })
-end
-
 -- Treat *.pdf.erb like *.html.erb for syntax highlighting.
 vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
   group = "jason-config",
