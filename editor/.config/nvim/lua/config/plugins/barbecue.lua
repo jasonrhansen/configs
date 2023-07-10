3dbf05cea49393ffb122c0d913b4f81e353690a0
Jason Rodney Hansen <jasonrodneyhansen@gmail.com>
2023-07-09 18:32:44 -0600

    Changes to barbecue config

--- editor/.config/nvim/lua/config/plugins/barbecue.lua
+++ editor/.config/nvim/lua/config/plugins/barbecue.lua
@@ -6,5 +6,39 @@ return {
     "SmiteshP/nvim-navic",
     "nvim-tree/nvim-web-devicons",
   },
-  opts = {},
+  config = function()
+    require("barbecue").setup({
+      create_autocmd = false,
+      show_modified = true,
+      theme = {
+        basename = { bold = true, italic = true },
+      },
+    })
+
+    vim.api.nvim_create_autocmd("LspAttach", {
+      desc = "Navic Attacher",
+      group = vim.api.nvim_create_augroup("barbecue.navic_attacher", {}),
+      callback = function(a)
+        local client = vim.lsp.get_client_by_id(a.data.client_id)
+        if client.server_capabilities["documentSymbolProvider"] then
+          require("nvim-navic").attach(client, a.buf)
+        end
+      end,
+    })
+
+    -- This will have better performance when moving the cursor than the default
+    -- autocommand, because we're not running it on every CursorMoved event.
+    vim.api.nvim_create_autocmd({
+      "WinResized",
+      "BufWinEnter",
+      "CursorHold",
+      "InsertLeave",
+      "BufModifiedSet",
+    }, {
+      group = vim.api.nvim_create_augroup("barbecue.updater", {}),
+      callback = function()
+        require("barbecue.ui").update()
+      end,
+    })
+  end,
 }
