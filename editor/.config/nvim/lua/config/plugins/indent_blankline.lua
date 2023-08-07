9c291cc42507e17bad3f318bd93392bc53743b0a
Jason Rodney Hansen <jasonrodneyhansen@gmail.com>
2023-08-06 18:34:29 -0600

    Indent blankline config changes

--- editor/.config/nvim/lua/config/plugins/indent_blankline.lua
+++ editor/.config/nvim/lua/config/plugins/indent_blankline.lua
@@ -7,8 +7,10 @@ function M.config()
   require("indent_blankline").setup({
     char = "│",
     use_treesitter = true,
+    use_treesitter_scope = false,
     show_first_indent_level = true,
-    show_current_context = false,
+    show_current_context = true,
+    show_current_context_start = false,
     show_trailing_blankline_indent = false,
     filetype_exclude = { "help", "packer" },
     buftype_exclude = { "help", "terminal", "nofile" },
