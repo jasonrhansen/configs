84762011fc2cfa8753721ab8e4bfd5b43b3b0a9a
Jason Rodney Hansen <jasonrodneyhansen@gmail.com>
2023-10-30 16:58:50 -0600

    Upgrade to window-picker v2

--- editor/.config/nvim/lua/config/plugins/window_picker.lua
+++ editor/.config/nvim/lua/config/plugins/window_picker.lua
@@ -1,46 +1,85 @@
 local M = {
   "s1n7ax/nvim-window-picker",
-  -- Stay on version 1 until the following issue is resolved:
-  -- https://github.com/s1n7ax/nvim-window-picker/issues/36
-  version = "1.*",
+  version = "2.*",
   event = "VeryLazy",
 }
 
 function M.config()
   require("window-picker").setup({
-    -- when there is only one window available to pick from, use that window
-    -- without prompting the user to select
-    autoselect_one = true,
-
-    -- whether you want to include the window you are currently on to window
-    -- selection or not
-    include_current_win = false,
+    -- type of hints you want to get
+    -- following types are supported
+    -- 'statusline-winbar' | 'floating-big-letter'
+    -- 'statusline-winbar' draw on 'statusline' if possible, if not 'winbar' will be
+    -- 'floating-big-letter' draw big letter on a floating window
+    -- used
+    hint = "statusline-winbar",
 
     -- when you go to window selection mode, status bar will show one of
     -- following letters on them so you can use that letter to select the window
     selection_chars = "FJDKSLA;CMRUEIWOQP",
 
+    -- This section contains picker specific configurations
+    picker_config = {
+      statusline_winbar_picker = {
+        -- You can change the display string in status bar.
+        -- It supports '%' printf style. Such as `return char .. ': %f'` to display
+        -- buffer file path. See :h 'stl' for details.
+        selection_display = function(char, windowid)
+          return "%=" .. char .. "%="
+        end,
+
+        -- whether you want to use winbar instead of the statusline
+        -- "always" means to always use winbar,
+        -- "never" means to never use winbar
+        -- "smart" means to use winbar if cmdheight=0 and statusline if cmdheight > 0
+        use_winbar = "never", -- "always" | "never" | "smart"
+      },
+
+      floating_big_letter = {
+        -- window picker plugin provides bunch of big letter fonts
+        -- fonts will be lazy loaded as they are being requested
+        -- additionally, user can pass in a table of fonts in to font
+        -- property to use instead
+
+        font = "ansi-shadow", -- ansi-shadow |
+      },
+    },
+
+    -- whether to show 'Pick window:' prompt
+    show_prompt = true,
+
+    -- prompt message to show to get the user input
+    prompt_message = "Pick window: ",
+
     -- if you want to manually filter out the windows, pass in a function that
-    -- takes two parameters. you should return window ids that should be
+    -- takes two parameters. You should return window ids that should be
     -- included in the selection
     -- EX:-
     -- function(window_ids, filters)
-    --    -- filter the window_ids
+    --    -- folder the window_ids
     --    -- return only the ones you want to include
     --    return {1000, 1001}
     -- end
     filter_func = nil,
 
     -- following filters are only applied when you are using the default filter
-    -- defined by this plugin. if you pass in a function to "filter_func"
+    -- defined by this plugin. If you pass in a function to "filter_func"
     -- property, you are on your own
     filter_rules = {
+      -- when there is only one window available to pick from, use that window
+      -- without prompting the user to select
+      autoselect_one = true,
+
+      -- whether you want to include the window you are currently on to window
+      -- selection or not
+      include_current_win = false,
+
       -- filter using buffer options
       bo = {
         -- if the file type is one of following, the window will be ignored
-        filetype = { "NvimTree", "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame" },
+        filetype = { "NvimTree", "neo-tree", "notify", "qf", "diff", "fugitive", "fugitiveblame" },
 
-        -- if the buffer type is one of following, the window will be ignored
+        -- if the file type is one of following, the window will be ignored
         buftype = { "nofile", "terminal", "help" },
       },
 
@@ -56,13 +95,34 @@ function M.config()
       file_name_contains = {},
     },
 
-    -- if you have include_current_win == true, then current_win_hl_color will
-    -- be highlighted using this background color
-    current_win_hl_color = "#e35e4f",
-
-    -- all the windows except the curren window will be highlighted using this
-    -- color
-    other_win_hl_color = "#54546d",
+    -- You can pass in the highlight name or a table of content to set as
+    -- highlight
+    highlights = {
+      statusline = {
+        focused = {
+          fg = "#ededed",
+          bg = "#e35e4f",
+          bold = true,
+        },
+        unfocused = {
+          fg = "#ededed",
+          bg = "#54546d",
+          bold = true,
+        },
+      },
+      winbar = {
+        focused = {
+          fg = "#ededed",
+          bg = "#e35e4f",
+          bold = true,
+        },
+        unfocused = {
+          fg = "#ededed",
+          bg = "#54546d",
+          bold = true,
+        },
+      },
+    },
   })
 end
 
--- editor/.config/nvim/lua/util.lua
+++ editor/.config/nvim/lua/util.lua
@@ -5,7 +5,7 @@ M.pick_window = function(func_or_cmd)
     local buf = vim.api.nvim_get_current_buf()
     local cursor = vim.api.nvim_win_get_cursor(0)
     local picked_win = require("window-picker").pick_window()
-    if picked_win then
+    if picked_win and picked_win ~= vim.api.nvim_get_current_win() then
       vim.api.nvim_set_current_win(picked_win)
       vim.api.nvim_set_current_buf(buf)
       vim.api.nvim_win_set_cursor(picked_win, cursor)
