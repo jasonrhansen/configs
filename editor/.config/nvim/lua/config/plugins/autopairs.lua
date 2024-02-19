433255b464dea9c98754a6538da3d2a68f54349c
Jason Rodney Hansen <jasonrodneyhansen@gmail.com>
2024-02-18 19:43:49 -0700

    Temporary workaround for nvim-autopairs bug

--- editor/.config/nvim/lua/config/plugins/autopairs.lua
+++ editor/.config/nvim/lua/config/plugins/autopairs.lua
@@ -1,5 +1,8 @@
 return {
   "windwp/nvim-autopairs",
+  -- Using this specific commit until the following issue is resolved:
+  -- https://github.com/windwp/nvim-autopairs/issues/430
+  commit = "00def0123a1a728c313a7dd448727eac71392c57",
   config = function()
     local autopairs = require("nvim-autopairs")
     autopairs.setup({})
