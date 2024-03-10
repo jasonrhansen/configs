0cdfb22484f948485fdcd8c1d2cbe4c8cc509588
Jason Rodney Hansen <jasonrodneyhansen@gmail.com>
2024-03-10 12:18:35 -0600

    Customize the Kanagawa theme

--- editor/.config/nvim/lua/config/plugins/kanagawa.lua
+++ editor/.config/nvim/lua/config/plugins/kanagawa.lua
@@ -16,12 +16,25 @@ return {
       overrides = function(colors)
         local theme = colors.theme
         return {
+          NormalFloat = { bg = "none" },
+          FloatBorder = { bg = "none" },
+          FloatTitle = { bg = "none" },
+
           Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 },
           PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
           PmenuSbar = { bg = theme.ui.bg_m1 },
           PmenuThumb = { bg = theme.ui.bg_p2 },
+
+          NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+
+          LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+
+          MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+
           TelescopeNormal = { bg = theme.ui.bg_m1 },
           TelescopeBorder = { bg = theme.ui.bg_m1 },
+
+          NeoTreeFloatTitle = { fg = theme.ui.special, bg = "none" },
         }
       end,
     })
