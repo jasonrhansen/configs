e24c7526da93aaaa6712f4fd715a39d2e4fe5bfa
Jason Rodney Hansen <jasonrodneyhansen@gmail.com>
2024-05-18 13:19:46 -0600

    Remove nvim-treesitter-playground (deprecated)

--- editor/.config/nvim/lua/config/plugins/treesitter.lua
+++ editor/.config/nvim/lua/config/plugins/treesitter.lua
@@ -7,7 +7,6 @@ local M = {
     end
   end,
   dependencies = {
-    "nvim-treesitter/playground",
     "nvim-treesitter/nvim-treesitter-textobjects",
     "nvim-treesitter/nvim-treesitter-context",
   },
@@ -87,24 +86,6 @@ function M.config()
         goto_previous_end = {
           ["[M"] = "@function.outer",
         },
-      },
-    },
-    playground = {
-      enable = true,
-      disable = disable,
-      updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-      persist_queries = false, -- Whether the query persists across vim sessions
-      keybindings = {
-        toggle_query_editor = "o",
-        toggle_hl_groups = "i",
-        toggle_injected_languages = "t",
-        toggle_anonymous_nodes = "a",
-        toggle_language_display = "I",
-        focus_language = "f",
-        unfocus_language = "F",
-        update = "R",
-        goto_node = "<cr>",
-        show_help = "?",
       },
     },
     query_linter = {
