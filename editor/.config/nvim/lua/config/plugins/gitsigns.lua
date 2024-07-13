a400155e12a17c21d6dab200b722fabee2763c66
Jason Rodney Hansen <jasonrodneyhansen@gmail.com>
2024-07-13 15:03:15 -0600

    Set current line blame delay to 500 ms

--- editor/.config/nvim/lua/config/plugins/gitsigns.lua
+++ editor/.config/nvim/lua/config/plugins/gitsigns.lua
@@ -20,6 +20,9 @@ function M.config()
       interval = 1000,
     },
     current_line_blame = true,
+    current_line_blame_opts = {
+      delay = 500,
+    },
     sign_priority = 6,
     update_debounce = 100,
     status_formatter = nil, -- Use default
