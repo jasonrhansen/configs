16f059c6ca39e285c830661f2ee743471aa48b6d
Jason Rodney Hansen <jasonrodneyhansen@gmail.com>
2024-07-20 13:43:42 -0600

    Increase which-key delay

--- editor/.config/nvim/lua/config/plugins/which_key.lua
+++ editor/.config/nvim/lua/config/plugins/which_key.lua
@@ -10,6 +10,12 @@ function M.config()
   wk.setup({
     ---@type false | "classic" | "modern" | "helix"
     preset = "modern",
+    -- Delay before showing the popup. Can be a number or a function that returns a number.
+    ---@type number | fun(ctx: { keys: string, mode: string, plugin?: string }):number
+    delay = function(ctx)
+      return ctx.plugin and 0 or 500
+    end,
+    notify = true,
   })
 end
 
