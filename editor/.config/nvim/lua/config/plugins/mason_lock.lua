00a8d12ff667da0ffcfa7bd075e42dd9588ab434
Jason Rodney Hansen <jasonrodneyhansen@gmail.com>
2024-11-29 10:23:31 -0700

    Remove mason lock

--- editor/.config/nvim/lua/config/plugins/mason_lock.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "zapling/mason-lock.nvim",
-
-  init = function()
-    require("mason-lock").setup({
-      lockfile_path = vim.fn.stdpath("config") .. "/mason-lock.json",
-    })
-  end,
-}
