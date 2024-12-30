5c17d2ae54af56e9bb83c1a03ecc84f56116a10c
Jason Rodney Hansen <jasonrodneyhansen@gmail.com>
2024-12-30 16:49:53 -0700

    Set max lines to 3 for treesitter context

--- editor/.config/nvim/lua/config/plugins/treesitter.lua
+++ editor/.config/nvim/lua/config/plugins/treesitter.lua
@@ -99,8 +99,8 @@ function M.config()
   })
 
   require("treesitter-context").setup({
-    enable = false, -- Enable this plugin (Can be enabled/disabled later via commands)
-    max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+    enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+    max_lines = 3, -- How many lines the window should span. Values <= 0 mean no limit.
     trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
     patterns = { -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
       -- For all filetypes
