91ef975ddb94432b4dc78d06cf8a0e4c0d2d90e1
Jason Rodney Hansen <jasonrodneyhansen@gmail.com>
2025-01-14 15:58:07 -0700

    Change dropbar config

--- editor/.config/nvim/lua/config/plugins/dropbar.lua
+++ editor/.config/nvim/lua/config/plugins/dropbar.lua
@@ -6,7 +6,24 @@ return {
     build = "make",
   },
   config = function()
-    require("dropbar").setup({
+    local dropbar = require("dropbar")
+    local sources = require("dropbar.sources")
+
+    vim.api.nvim_set_hl(0, "DropBarFileName", { fg = "#FFFFFF", italic = true })
+
+    local custom_path = {
+      get_symbols = function(buff, win, cursor)
+        local symbols = sources.path.get_symbols(buff, win, cursor)
+        symbols[#symbols].name_hl = "DropBarFileName"
+        if vim.bo[buff].modified then
+          symbols[#symbols].name = symbols[#symbols].name .. " [+]"
+          symbols[#symbols].name_hl = "DiffAdded"
+        end
+        return symbols
+      end,
+    }
+
+    dropbar.setup({
       icons = {
         kinds = {
           dir_icon = "",
@@ -16,6 +33,25 @@ return {
         keymaps = {
           ["gq"] = "<c-w>q",
         },
+      },
+      bar = {
+        sources = function(buf, _)
+          if vim.bo[buf].ft == "markdown" then
+            return {
+              custom_path,
+              sources.markdown,
+            }
+          end
+          if vim.bo[buf].buftype == "terminal" then
+            return {
+              sources.terminal,
+            }
+          end
+          return {
+            custom_path,
+          }
+        end,
+        truncate = false;
       },
     })
 
