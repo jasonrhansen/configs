52f3f70b9b4d05e9e04fee8635d5e4142c9cecfd
Jason Rodney Hansen <jasonrodneyhansen@gmail.com>
2025-02-23 11:55:41 -0700

    Auto show completion documentation

--- editor/.config/nvim/lua/config/plugins/blink-cmp.lua
+++ editor/.config/nvim/lua/config/plugins/blink-cmp.lua
@@ -6,6 +6,10 @@ return {
       ghost_text = {
         enabled = true,
       },
+      documentation = {
+        auto_show = true,
+        auto_show_delay_ms = 500,
+      },
     },
 
     keymap = {
