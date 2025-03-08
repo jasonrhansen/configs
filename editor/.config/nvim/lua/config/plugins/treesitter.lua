bd7ca8b24cbd51efc75924812e116e6e8bce7268
Jason Rodney Hansen <jasonrodneyhansen@gmail.com>
2025-03-08 09:50:41 -0700

    Disable treesitter indent for rust
    Auto indenting blocks after newline doesn't work correctly with it
    enabled for some reaseon.

--- editor/.config/nvim/lua/config/plugins/treesitter.lua
+++ editor/.config/nvim/lua/config/plugins/treesitter.lua
@@ -33,7 +33,7 @@ function M.config()
   end
 
   local disable_indent = function(lang, bufnr)
-    return lang == "ruby" or disable(lang, bufnr)
+    return lang == "ruby" or lang == "rust" or disable(lang, bufnr)
   end
 
   treesitter.setup({
