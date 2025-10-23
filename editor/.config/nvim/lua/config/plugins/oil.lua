905df4d301c261cbdbc1d6b2731085407dffe29f
Jason Rodney Hansen <jasonrodneyhansen@gmail.com>
2025-10-23 15:08:56 -0600

    Oil config changes

--- editor/.config/nvim/lua/config/plugins/oil.lua
+++ editor/.config/nvim/lua/config/plugins/oil.lua
@@ -20,6 +20,40 @@ return {
         },
         ["<esc>"] = { "actions.close", mode = "n" },
         ["gq"] = { "actions.close", mode = "n" },
+        ["<leader>y"] = { "actions.yank_entry", mode = "n" },
+        ["<leader>Y"] = {
+          desc = "Yank the path of the current oil directory to a register",
+          mode = "n",
+          callback = function()
+            local dir = oil.get_current_dir()
+            vim.fn.setreg(vim.v.register, dir)
+          end,
+        },
+        ["<c-w>"] = {
+          desc = "Open with window picker",
+          buffer = true,
+          nowait = true,
+          callback = function()
+            local entry = oil.get_cursor_entry()
+            if not entry or entry.type ~= "file" then
+              return
+            end
+            local dir = oil.get_current_dir()
+            oil.close({ exit_if_last_buf = false })
+            local win = require("window-picker").pick_window({
+              filter_rules = {
+                autoselect_one = true,
+                include_current_win = true,
+              },
+            })
+
+            if win then
+              vim.api.nvim_set_current_win(win)
+              local path = vim.fs.joinpath(dir, entry.name)
+              vim.cmd.edit(path)
+            end
+          end,
+        },
       },
       float = {
         max_width = 100,
@@ -27,35 +61,5 @@ return {
       },
     })
     vim.keymap.set("n", "-", "<cmd>Oil --float<cr>", { desc = "Open directory in oil" })
-    vim.api.nvim_create_autocmd({ "FileType" }, {
-      group = "jason-config",
-      pattern = { "oil" },
-      callback = function()
-        vim.keymap.set("n", "<c-w>", function()
-          local entry = oil.get_cursor_entry()
-          if not entry or entry.type ~= "file" then
-            return
-          end
-          local dir = oil.get_current_dir()
-          oil.close({ exit_if_last_buf = false })
-          local win = require("window-picker").pick_window({
-            filter_rules = {
-              autoselect_one = true,
-              include_current_win = true,
-            },
-          })
-
-          if win then
-            vim.api.nvim_set_current_win(win)
-            local path = vim.fs.joinpath(dir, entry.name)
-            vim.cmd.edit(path)
-          end
-        end, {
-          desc = "Open with window picker",
-          buffer = true,
-          nowait = true,
-        })
-      end,
-    })
   end,
 }
