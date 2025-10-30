277a4f570dea417c5a26b60434a74b3e09c7ae98
Jason Rodney Hansen <jasonrodneyhansen@gmail.com>
2025-10-30 16:47:56 -0600

    Integrate Snacks.rename with oil.nvim

--- editor/.config/nvim/lua/config/plugins/snacks.lua
+++ editor/.config/nvim/lua/config/plugins/snacks.lua
@@ -492,5 +492,15 @@ return {
         Snacks.toggle.scroll():map("<leader>tS")
       end,
     })
+
+    -- LSP-integrated file renaming, when renaming files in oil.nvim.
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "OilActionsPost",
+      callback = function(event)
+        if event.data.actions[1].type == "move" then
+          Snacks.rename.on_rename_file(event.data.actions[1].src_url, event.data.actions[1].dest_url)
+        end
+      end,
+    })
   end,
 }
