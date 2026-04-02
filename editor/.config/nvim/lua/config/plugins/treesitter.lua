2df37efadb348274d61e432092c3ee9e07857b0c
Jason Rodney Hansen <jasonrodneyhansen@gmail.com>
2026-04-02 13:24:21 -0600

    More treesitter fixes

--- editor/.config/nvim/lua/config/plugins/treesitter.lua
+++ editor/.config/nvim/lua/config/plugins/treesitter.lua
@@ -1,33 +1,39 @@
 local M = {
-	"nvim-treesitter/nvim-treesitter",
-	lazy = false,
-	build = ":TSUpdate",
+  "nvim-treesitter/nvim-treesitter",
+  lazy = false,
+  build = ":TSUpdate",
 }
 
 function M.config()
-	local default_no_highlight_langs = { "html" }
+  local enable_highlights = function(lang)
+    return not vim.tbl_contains({ "html" }, lang)
+  end
 
-	vim.api.nvim_create_autocmd("FileType", {
-		callback = function(args)
-			local bufnr = args.buf
-			local ft = vim.bo[bufnr].filetype
-			local lang = vim.treesitter.language.get_lang(ft) or ft
+  local enable_indent = function(lang)
+    return not vim.tbl_contains({ "ruby", "rust" }, lang)
+  end
 
-			if vim.tbl_contains(default_no_highlight_langs, lang) then
-				return
-			end
+  vim.api.nvim_create_autocmd("FileType", {
+    callback = function(args)
+      local bufnr = args.buf
+      local ft = vim.bo[bufnr].filetype
+      local lang = vim.treesitter.language.get_lang(ft) or ft
 
-			-- Check if a parser is actually installed for this language
-			local has_parser = pcall(vim.treesitter.get_parser, bufnr, lang)
+      -- Check if a parser is actually installed for this language
+      local has_parser = pcall(vim.treesitter.get_parser, bufnr, lang)
 
-			-- Only attempt to start if the parser exists
-			if has_parser then
-				-- Use pcall here too, just in case the parser is corrupted
-				-- or the buffer is in an invalid state
-				pcall(vim.treesitter.start, bufnr, lang)
-			end
-		end,
-	})
+      -- Only attempt to start if the parser exists
+      if has_parser then
+        if enable_highlights(lang) then
+          pcall(vim.treesitter.start, bufnr, lang)
+        end
+
+        if enable_indent(lang) then
+          vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end
+      end
+    end,
+  })
 end
 
 return M
