c1326c589aecb43b12cf1b043478975f977767af
Jason Rodney Hansen <jasonrodneyhansen@gmail.com>
2026-04-05 09:24:00 -0600

    Update blink config

--- editor/.config/nvim/lua/config/plugins/blink-cmp.lua
+++ editor/.config/nvim/lua/config/plugins/blink-cmp.lua
@@ -2,28 +2,36 @@ return {
   "saghen/blink.cmp",
   version = "*",
   opts = {
+    appearance = {
+      use_nvim_cmp_as_default = true,
+      nerd_font_variant = "mono",
+    },
     completion = {
-      ghost_text = {
-        enabled = true,
-      },
+      ghost_text = { enabled = true },
       documentation = {
         auto_show = true,
-        auto_show_delay_ms = 500,
+        auto_show_delay_ms = 200,
+        window = { border = "rounded" },
       },
       menu = {
         border = "none",
         draw = {
-          -- We don't need label_description now because label and label_description are already
-          -- combined together in label by colorful-menu.nvim.
-          columns = { { "kind_icon" }, { "label", gap = 1 } },
+          treesitter = { "lsp" },
+          columns = {
+            { "kind_icon" },
+            { "label", "label_description", gap = 1 },
+            { "kind", gap = 1 },
+          },
           components = {
+            kind = {
+              highlight = "Comment",
+            },
+            label_description = {
+              width = { max = 30 },
+              highlight = "NonText",
+            },
             label = {
-              text = function(ctx)
-                return require("colorful-menu").blink_components_text(ctx)
-              end,
-              highlight = function(ctx)
-                return require("colorful-menu").blink_components_highlight(ctx)
-              end,
+              width = { max = 40 },
             },
           },
         },
@@ -40,37 +48,20 @@ return {
       },
     },
 
-    cmdline = {
-      enabled = false,
-    },
+    signature = { enabled = true, window = { border = "rounded" } },
 
-    signature = {
-      enabled = true,
-    },
-
-    appearance = {
-      -- Sets the fallback highlight groups to nvim-cmp's highlight groups
-      -- Useful for when your theme doesn't support blink.cmp
-      -- Will be removed in a future release
-      use_nvim_cmp_as_default = true,
-      -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-      -- Adjusts spacing to ensure icons are aligned
-      nerd_font_variant = "mono",
-    },
-
-    -- Default list of enabled providers defined so that you can extend it
-    -- elsewhere in your config, without redefining it, due to `opts_extend`
     sources = {
       default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+      per_filetype = {
+        codecompanion = { "codecompanion", "snippets", "buffer" },
+      },
       providers = {
         lazydev = {
           name = "LazyDev",
           module = "lazydev.integrations.blink",
-          -- make lazydev completions top priority (see `:h blink.cmp`)
           score_offset = 100,
         },
       },
     },
   },
-  opts_extend = { "sources.default" },
 }
